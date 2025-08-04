56c5ebb625edf2e50124842f228b9106d9a4f688
onurdincel <s.onurdincel@gmail.com>
2025-08-04 18:41:11 +0300

    some futures added

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,16 @@
 from textnode import TextNode, TextType
 import re
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -85,3 +95,4 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
