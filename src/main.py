532cb41a611af730f83e1d3a774e7ea513225d8c
onurdincel <s.onurdincel@gmail.com>
2025-08-06 03:59:02 +0300

    add few things

--- src/main.py
+++ src/main.py
@@ -1,4 +1,27 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-print(node)
+def copy_directory_contents(source, destination):
+    if os.path.exists(destination):
+        print(f"Deleting: {destination}")
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    print(f"Creating: {destination}")
+
+    for item in os.listdir(source):
+        src_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(f"Copy file: {dest_path}")
+        elif os.path.isdir(src_path):
+            copy_directory_contents(src_path, dest_path)
+
+
+
+def main():
+    copy_directory_contents("static", "public")
+
+if __name__ == "__main__":
+    main()
